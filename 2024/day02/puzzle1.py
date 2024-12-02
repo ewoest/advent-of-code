2a44847b734bd0badc80f2e1116bd2deea6f5797
Eric Woestman <eric.woestman@gmail.com>
2024-12-02 08:53:02 -0600

    2024 Day 02

---
+++ 2024/day02/puzzle1.py
@@ -0,0 +1,38 @@
+import os, sys
+
+def is_report_valid(nums):
+    for i in range(len(nums)-1):
+        diff = nums[i+1] - nums[i]
+
+        if diff < 1 or diff > 3:
+            return False
+    
+    return True
+
+def solve(filename):
+    lines = read_file_lines(filename)
+
+    count = 0
+    
+    for line in lines:
+        nums = [int(x) for x in line.split()]
+
+        if nums[-1] < nums[0]:
+            nums.reverse()
+
+        if is_report_valid(nums):
+            count = count + 1
+
+    print(f'count: {count}')
+
+
+def read_file_lines(filename):
+    script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+    with open(script_directory + "/" + filename) as f:
+        lines = f.read().splitlines()
+        return lines
+
+
+if __name__ == '__main__':
+    solve('example.txt')
+    solve('input.txt')
